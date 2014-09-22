f8c6fd12d407b8d7e2a19aaf5decc574c11ba9c9
ChapmanDS1 <ChapmanDS1@gcc.edu>
2014-09-22 16:23:47 -0400

    1.1.5
    The shuffle function now randomizes everything very nicely

--- Hearts/Hearts/Deck.cpp
+++ Hearts/Hearts/Deck.cpp
@@ -22,11 +22,13 @@ Deck::Deck()
 
 void Deck::shuffle(int number)
 {
+	srand(time(NULL));
 	vector<Card> deck1 = deck;
 
 	int x = 0;
 	while(x <number)
 	{
+		
 		int counter = 0;
 		for(int i = 0; i < number_of_cards; i +=2)
 		{
@@ -43,6 +45,22 @@ void Deck::shuffle(int number)
 		}
 		x++;
 		deck1 = deck;
+
+		
+		//Swaps a random amount of cards around to random positions. If this isn't present
+		//The two of clubs will always be on top no matter how many times you shuffle
+		for(int i = rand()%20; i<30; i++)
+		{
+			int random = rand() % 52;
+			int random1= rand() % 52;
+			if(random != random1)
+			{
+				Card temp;
+				temp = deck[random];
+				deck[random] = deck[random1];
+				deck[random1] = temp;
+			}
+		}
 	}
 
 }
--- Hearts/Hearts/Deck.h
+++ Hearts/Hearts/Deck.h
@@ -1,11 +1,15 @@
 #include "Card.h"
 #include <vector>
 using std::vector;
+#include <cstdlib>
+using std::rand;
+#include <ctime>
+using std::time;
 
 class Deck{
 public:
 	Deck();
-	void shuffle(int number);
+	void shuffle(int number);  // number = The number of times to run the shuffle algorithm (3-5 works well) Although the more you run it the more varied it gets
 	Card deal();
 
 private:

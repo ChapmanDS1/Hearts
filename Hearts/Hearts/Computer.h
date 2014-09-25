0e53e36e257e5fcfc09b132d2165649ac1377ecf
coopersj1 <coopersj1@gcc.edu>
2014-09-25 15:24:56 -0400

    .5.2.3
    hopefully fixed discrepancies.

--- Hearts/Hearts/Computer.h
+++ Hearts/Hearts/Computer.h
@@ -8,12 +8,9 @@ class Computer
 public:
 	Computer();
 	Card playCard(vector<Card> trick);
-<<<<<<< HEAD
 	void addCardToHand(vector<Card> cards);
-=======
 	int getScore(){return score;}
 	void addScore(int points){score += points;}
->>>>>>> d49dca4f8a930b9c8bd5e540d7233d6342f41a3b
 private:
 	vector<Card> hand;
 	int score;
